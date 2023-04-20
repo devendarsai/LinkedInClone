c45198b3345e90018ac0e3dfa891f326342381ac
Devendar Sai Thallam <programmingwithdev@gmail.com>
2023-04-20 14:29:58 -0700

    Login Page - working condition

--- linkedin-clone/src/api/AuthAPI.jsx
+++ linkedin-clone/src/api/AuthAPI.jsx
@@ -1,1 +1,22 @@
-export const LoginAPI = () => {};
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+
+export const LoginAPI = (email, password) => {
+    // console.log("Hello from Login API");
+    // return "Hello from Login API";
+    try {
+        let response = signInWithEmailAndPassword(auth, email, password);
+        return response;
+    } catch (err) {
+        return err;
+    }
+};
+
+export const RegisterAPI = (email, password) => {
+    try {
+        let response = createUserWithEmailAndPassword(auth, email, password);
+        return response;
+    } catch (err) {
+        return err;
+    }
+};
--- linkedin-clone/src/components/LoginComponent.jsx
+++ linkedin-clone/src/components/LoginComponent.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { LoginAPI } from "../api/AuthAPI";
+import "../Sass/LoginComponent.scss";
+import LinkedinLogo from "../assets/linkedinLogo.png";
 
 export default function LoginComponent() {
+    const [credentials, setCredentials] = useState({});
+    const login = async () => {
+        try {
+            let res = await LoginAPI(credentials.email, credentials.password);
+            console.log(res?.user);
+        } catch (err) {
+            console.log(err.errors.message);
+        }
+    };
     return (
-        <div>
-            <h1>LoginComponent</h1>
+        <div className="login-component">
+            <div className="login-wrapper">
+                <img src={LinkedinLogo} className="linkedinLogo" />
+                <div className="login-wrapper-inner">
+                    <h1 className="heading">Sign in</h1>
+                    <p className="sub-heading">Stay updated on your professional world</p>
+
+                    <div className="auth-inputs">
+                        <input
+                            onChange={(event) =>
+                                setCredentials({ ...credentials, email: event.target.value })
+                            }
+                            type="email"
+                            className="common-input"
+                            placeholder="Email or Phone"
+                        />
+                        <input
+                            onChange={(event) =>
+                                setCredentials({ ...credentials, password: event.target.value })
+                            }
+                            type="password"
+                            className="common-input"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <a href="#" className="forgot-password">
+                        {" "}
+                        Forgot password?
+                    </a>
+                    <button onClick={login} className="login-btn">
+                        Sign in
+                    </button>
+                    <p className="new-to-linkedin">
+                        New to LinkedIn?{" "}
+                        <a href="#" className="join-now">
+                            Join now
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div className="login-footer">
+                <div className="login-footer-inner">
+                    <div className="footer-links">
+                        <a href="#">About</a>
+                        <a href="#">Accessibility</a>
+                        <a href="#">Help Center</a>
+                        <a href="#">Privacy & Terms</a>
+                        <a href="#">Ad Choices</a>
+                        <a href="#">Advertising</a>
+                        <a href="#">Business Services</a>
+                        <a href="#">Get the LinkedIn app</a>
+                        <a href="#">More</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
